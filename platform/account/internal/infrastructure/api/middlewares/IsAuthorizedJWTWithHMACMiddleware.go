3716020a8890c9e4b29649985c565279b896fa8d
koubae <58447627+Koubae@users.noreply.github.com>
2025-07-01 06:21:28 +0200

    Created jwtMiddleware to unify the jwt auth logic

--- platform/account/internal/infrastructure/api/middlewares/IsAuthorizedJWTWithHMACMiddleware.go
+++ platform/account/internal/infrastructure/api/middlewares/IsAuthorizedJWTWithHMACMiddleware.go
@@ -1,42 +1,12 @@
 package middlewares
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
-	"net/http"
-	"strings"
 )
 
 func IsAuthorizedJWTWithHMACMiddleware(secret []byte) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Not Authorized -- missing"})
-			return
-		}
-
-		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-		token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
-			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method")
-			}
-			return secret, nil
-		})
-
-		if err != nil || !token.Valid {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized -- not valid"})
-			return
-		}
-
-		// Set claims to context
-		if claims, ok := token.Claims.(jwt.MapClaims); ok {
-			c.Set("user_id", claims["sub"])
-			c.Set("issuer", claims["iss"])
-			c.Set("role", claims["role"])
-			c.Set("client_id", claims["client_id"])
-		}
-
-		c.Next()
+		jwtMiddleware(c, jwt.SigningMethodHS256, secret)
 	}
 }
---
+++ platform/account/internal/infrastructure/api/middlewares/jwt_middleware.go
@@ -0,0 +1,57 @@
+package middlewares
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func jwtMiddleware(c *gin.Context, method jwt.SigningMethod, secret interface{}) {
+	authHeader := c.GetHeader("Authorization")
+	if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "missing or invalid token"})
+		return
+	}
+
+	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if t.Method != method {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return secret, nil
+	})
+
+	if err != nil || !token.Valid {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid claims"})
+		return
+	}
+
+	var exp float64
+	exp, ok = claims["exp"].(float64)
+	if !ok {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid claims exp"})
+		return
+	}
+
+	now := float64(time.Now().Unix())
+	if exp < now {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Token expired"})
+		return
+	}
+
+	c.Set("user_id", claims["sub"])
+	c.Set("issuer", claims["iss"])
+	c.Set("role", claims["role"])
+	c.Set("client_id", claims["client_id"])
+
+	c.Next()
+}
