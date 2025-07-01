5dae05f0196a70d51729b2491e99abb22935ff4a
koubae <58447627+Koubae@users.noreply.github.com>
2025-07-01 11:07:22 +0200

    Added required bindings

--- platform/account/internal/application/account/handlers/handlers.go
+++ platform/account/internal/application/account/handlers/handlers.go
@@ -5,7 +5,7 @@ import (
 )
 
 type GetAccountRequest struct {
-	FullProfile bool `form:"full_profile" json:"full_profile"`
+	FullProfile bool `form:"full_profile" json:"full_profile" binding:"required"`
 	Username    string
 	ClientID    string
 	UserID      uint
