e7f12278f053becd993c291dbbf8a1cc2d97ae1e
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-11 21:11:01 +0100

    Created first simple server

--- internal/app/server.go
+++ internal/app/server.go
@@ -1,9 +1,33 @@
 package app
 
 import (
+	"io"
 	"log"
+	"net/http"
+	"time"
 )
 
 func RunServer() {
-	log.Println("Server started")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(
+		"GET /{$}", func(w http.ResponseWriter, req *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			_, err := io.WriteString(w, "Hello World :)\n")
+			if err != nil {
+				return
+			}
+		},
+	)
+
+	srv := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		// Use CrossOriginProtection.Handler to block all non-safe cross-origin
+		// browser requests to mux.
+		Handler: http.NewCrossOriginProtection().Handler(mux),
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
