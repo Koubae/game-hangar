b8af521f2dadd95165d7ec18f8dfaa9b05ff1707
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-16 18:06:54 +0100

    Auth: RegisterByUsername uses dto validation

--- internal/identity/app/modules/auth/api/controller.go
+++ internal/identity/app/modules/auth/api/controller.go
@@ -1,13 +1,32 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/koubae/game-hangar/pkg/common"
 )
 
 type AuthController struct{}
 
+type RegisterByUsernameRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func (controller *AuthController) RegisterByUsername(w http.ResponseWriter, r *http.Request) {
+	var payload RegisterByUsernameRequest
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		http.Error(w, fmt.Sprintf("invalid json: %v", err), http.StatusBadRequest)
+		return
+	}
+	if err := common.DTOSchemaValidation(payload); err != nil {
+		http.Error(w, fmt.Sprintf("invalid payload: %v", err), http.StatusBadRequest)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
 	_, _ = io.WriteString(w, "ok RegisterByUsername\n")
 }
