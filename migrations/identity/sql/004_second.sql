074ad0e7125a57035668253ffa47239681a412e9
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-16 22:08:04 +0100

    Clean up migrations from 0

--- migrations/identity/sql/004_second.sql
+++
@@ -1,8 +0,0 @@
--- +migrate Up
-CREATE TABLE IF NOT EXISTS example_3 (
-    id         SERIAL PRIMARY KEY,
-    email      VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT NOW()
-);
--- +migrate Down
-DROP TABLE IF EXISTS example_3;
